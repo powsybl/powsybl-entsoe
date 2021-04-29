b00af7dcbe07f85a7b55302019685e15ae8da092
RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>
2021-04-29 10:33:21 +0200

    Modify artifactId, package, name and descriptions
    Signed-off-by: RALAMBOTIANA MIORA <miora.ralambotiana@rte-france.com>

--- entsoe-cgmes-balances-adjustment/src/main/java/com/powsybl/entsoe/cgmes/balances_adjustment/util/CgmesVoltageLevelsArea.java
+++ entsoe-cgmes-balances-adjustment/src/main/java/com/powsybl/entsoe/cgmes/balances_adjustment/util/CgmesVoltageLevelsArea.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.entsoe.balances_adjustment.util;
+package com.powsybl.entsoe.cgmes.balances_adjustment.util;
 
 import com.powsybl.balances_adjustment.util.NetworkArea;
 import com.powsybl.cgmes.extensions.CgmesControlArea;
