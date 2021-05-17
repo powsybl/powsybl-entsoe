d598d844a2c1b96539259aa54398d8f8d1e19d79
Coline Piloquet <coline.piloquet@rte-france.com>
2021-05-17 15:15:49 +0200

    Add fictitious Area code
    Signed-off-by: piloquetcol <coline.piloquet@rte-france.com>

---
+++ entsoe-cgmes-balances-adjustment/src/main/java/com/powsybl/entsoe/cgmes/balances_adjustment/util/CgmesFictitiousArea.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.entsoe.cgmes.balances_adjustment.util;
+
+import com.powsybl.balances_adjustment.util.NetworkArea;
+import com.powsybl.cgmes.extensions.CgmesControlArea;
+import com.powsybl.cgmes.extensions.CgmesDanglingLineBoundaryNode;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.DanglingLine;
+import com.powsybl.iidm.network.Network;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+class CgmesFictitiousArea implements NetworkArea {
+
+    private final Set<DanglingLine> danglingLinesCache;
+
+    CgmesFictitiousArea(Network network, List<CgmesControlArea> areas) {
+        danglingLinesCache = network.getDanglingLineStream()
+                .filter(dl -> dl.getExtension(CgmesDanglingLineBoundaryNode.class) == null || !dl.getExtension(CgmesDanglingLineBoundaryNode.class).isHvdc())
+                .filter(dl -> dl.getTerminal().getBusView().getBus() != null && dl.getTerminal().getBusView().getBus().isInMainSynchronousComponent())
+                .filter(dl -> areas.isEmpty() || areas.stream().anyMatch(area -> area.getTerminals().stream().anyMatch(t -> t.getConnectable().getId().equals(dl.getId()))
+                        || area.getBoundaries().stream().anyMatch(b -> b.getConnectable().getId().equals(dl.getId()))))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public double getNetPosition() {
+        return danglingLinesCache.parallelStream().mapToDouble(dl -> dl.getTerminal().isConnected() ? dl.getBoundary().getP() : 0).sum();
+    }
+
+    @Override
+    public Collection<Bus> getContainedBusViewBuses() {
+        return Collections.emptyList();
+    }
+}
