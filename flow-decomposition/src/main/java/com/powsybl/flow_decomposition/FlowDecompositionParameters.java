9960362af768dc103cca65a860c7e28fb2e07fc7
Sébastien Murgey <sebastien.murgey@rte-france.com>
2022-07-19 13:40:36 +0200

    Add flow decomposition parameters loading from powsybl config file
    Signed-off-by: Sébastien Murgey <sebastien.murgey@rte-france.com>

--- flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionParameters.java
+++ flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionParameters.java
@@ -6,6 +6,10 @@
  */
 package com.powsybl.flow_decomposition;
 
+import com.powsybl.commons.config.PlatformConfig;
+
+import java.util.Objects;
+
 /**
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
@@ -23,19 +27,41 @@ public class FlowDecompositionParameters {
     private static final boolean DEFAULT_ENABLE_LOSSES_COMPENSATION = DISABLE_LOSSES_COMPENSATION;
     private static final double DEFAULT_LOSSES_COMPENSATION_EPSILON = 1e-5;
     private static final double DEFAULT_SENSITIVITY_EPSILON = 1e-5;
-    private static final boolean DEFAULT_RESCALE_MODE = DISABLE_RESCALED_RESULTS;
+    private static final boolean DEFAULT_RESCALE_ENABLED = DISABLE_RESCALED_RESULTS;
     private boolean saveIntermediates;
     private boolean enableLossesCompensation;
     private double lossesCompensationEpsilon;
     private double sensitivityEpsilon;
     private boolean rescaleEnabled;
 
+    public static FlowDecompositionParameters load() {
+        return load(PlatformConfig.defaultConfig());
+    }
+
+    public static FlowDecompositionParameters load(PlatformConfig platformConfig) {
+        FlowDecompositionParameters parameters = new FlowDecompositionParameters();
+        load(parameters, platformConfig);
+        return parameters;
+    }
+
+    private static void load(FlowDecompositionParameters parameters, PlatformConfig platformConfig) {
+        Objects.requireNonNull(parameters);
+        Objects.requireNonNull(platformConfig);
+        platformConfig.getOptionalModuleConfig("flow-decomposition-default-parameters").ifPresent(moduleConfig -> {
+            parameters.setSaveIntermediates(moduleConfig.getBooleanProperty("save-intermediates", DEFAULT_SAVE_INTERMEDIATES));
+            parameters.setEnableLossesCompensation(moduleConfig.getBooleanProperty("enable-losses-compensation", DEFAULT_ENABLE_LOSSES_COMPENSATION));
+            parameters.setLossesCompensationEpsilon(moduleConfig.getDoubleProperty("losses-compensation-epsilon", DEFAULT_LOSSES_COMPENSATION_EPSILON));
+            parameters.setSensitivityEpsilon(moduleConfig.getDoubleProperty("sensitivity-epsilon", DEFAULT_SENSITIVITY_EPSILON));
+            parameters.setRescaleEnabled(moduleConfig.getBooleanProperty("rescale-enabled", DEFAULT_RESCALE_ENABLED));
+        });
+    }
+
     public FlowDecompositionParameters() {
         this.saveIntermediates = DEFAULT_SAVE_INTERMEDIATES;
         this.enableLossesCompensation = DEFAULT_ENABLE_LOSSES_COMPENSATION;
         this.lossesCompensationEpsilon = DEFAULT_LOSSES_COMPENSATION_EPSILON;
         this.sensitivityEpsilon = DEFAULT_SENSITIVITY_EPSILON;
-        this.rescaleEnabled = DEFAULT_RESCALE_MODE;
+        this.rescaleEnabled = DEFAULT_RESCALE_ENABLED;
     }
 
     public boolean doesSaveIntermediates() {
