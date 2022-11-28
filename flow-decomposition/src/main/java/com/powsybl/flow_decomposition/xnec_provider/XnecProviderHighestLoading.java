b86525dad2b2066430a4375101956618c9ad3aef
Alexandre Le Jean <alexandre.le-jean@artelys.com>
2022-11-28 15:44:53 +0100

    [WIP] feat: improved class XnecProviderHighestLoading based on remarks from PR
    Signed-off-by: Alexandre Le Jean <alexandre.le-jean@artelys.com>

--- flow-decomposition/src/main/java/com/powsybl/flow_decomposition/xnec_provider/XnecProviderHighestLoading.java
+++ flow-decomposition/src/main/java/com/powsybl/flow_decomposition/xnec_provider/XnecProviderHighestLoading.java
@@ -1,78 +1,48 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.flow_decomposition.xnec_provider;
 
+import com.powsybl.flow_decomposition.NetworkUtil;
 import com.powsybl.flow_decomposition.XnecProvider;
 import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.Identifiable;
 import com.powsybl.iidm.network.Network;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
- * This class selects for each XNE the associate XNEC with the highest absolute loading
- *
  * @author Alexandre LE JEAN
  */
-class XnecProviderHighestLoading implements XnecProvider {
-
-    final private List<Xnec> xnecs;
-
+public class XnecProviderHighestLoading implements XnecProvider {
     @Override
     public List<Branch> getNetworkElements(Network network) {
-        treatmentOfTieLine();
-        xnecSelection();
+        List<Branch<?>> xnecList = NetworkUtil.getAllValidBranches(network)
+                .stream()
+                .filter(XnecProviderInterconnection::isAnInterconnection)
+                .collect(Collectors.toList());
+
+        Set<String> xneIds = xnecList.stream()
+                .map(Identifiable::getNameOrId)
+                .collect(Collectors.toSet());
+
+        List<Branch> selection = new LinkedList<>();
+
+        for (String xneId : xneIds) {
+            Branch refBranch = getReferenceContingency(xneId, xnecList);
+            selection.add(refBranch);
+        }
+
+        return selection;
+    }
+
+    private Branch getReferenceContingency(String xneId, List<Branch<?>> xnecList) {
+        // TODO - implements
+        // check for materialized remedial action
+        // then, compare based on Active Power Flow
         return null;
-    }
-
-    /** Give the full list of overloaded XNEC before and after the RAO */
-    public XnecProviderHighestLoading(Collection<Xnec> xnecs) {
-        this.xnecs = new ArrayList<>(xnecs);
-    }
-
-    private void xnecSelection() {
-        // Sort XNECs : ascending order in name, then prioritize those with remedial action materialized,
-        // then by ascending order in active power flow before RAO
-        xnecs.sort((o1, o2) -> {
-            if(o1.xneNameOrId.compareTo(o2.xneNameOrId) != 0)
-                return o1.xneNameOrId.compareTo(o2.xneNameOrId);
-            else if(o1.isRemedialActionMaterialized || o2.isRemedialActionMaterialized)
-                return o1.isRemedialActionMaterialized ? -1 : 1;
-            else if (o1.activePowerFlowBeforeRao != o2.activePowerFlowBeforeRao)
-                return o1.activePowerFlowBeforeRao > o2.activePowerFlowBeforeRao ? -1 : 1;
-            else
-                return 0;
-        });
-
-        // For each XNEC, remove if previous XNEC has the same name or ID,
-        // i.e. no remedial action materialized or lower active power flow
-        final Iterator<Xnec> itr = xnecs.iterator();
-        String old = "";
-        while(itr.hasNext()) {
-            String next = itr.next().xneNameOrId;
-            if (next.equals(old)) {
-                itr.remove();
-            }
-            else {
-                old = next;
-            }
-        }
-    }
-
-    private void treatmentOfTieLine() {
-        int i = 0;
-        while (i < xnecs.size()) {
-            int j = i+1;
-            while (j < xnecs.size()){
-                if(xnecs.get(i).tsoName1.equals(xnecs.get(j).tsoName1)
-                        && (xnecs.get(i).nodeNameOrId1.equals(xnecs.get(j).nodeNameOrId1) ||
-                        xnecs.get(i).nodeNameOrId1.equals(xnecs.get(j).nodeNameOrId2) ||
-                        xnecs.get(i).nodeNameOrId2.equals(xnecs.get(j).nodeNameOrId1) ||
-                        xnecs.get(i).nodeNameOrId2.equals(xnecs.get(j).nodeNameOrId2))) {
-                    xnecs.get(i).tsoName2 = xnecs.get(j).tsoName1;
-                    xnecs.get(i).flowMax2 = xnecs.get(j).flowMax1;
-                    xnecs.remove(j);
-                }
-                else
-                    j++;
-            }
-            i++;
-        }
     }
 }
