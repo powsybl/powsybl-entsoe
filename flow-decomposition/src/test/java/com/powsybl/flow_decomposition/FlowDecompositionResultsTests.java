9f2f61450b0d858034870d176fba75a455bb8d6e
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-12-05 13:40:46 +0100

    Init new tests
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

---
+++ flow-decomposition/src/test/java/com/powsybl/flow_decomposition/FlowDecompositionResultsTests.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.flow_decomposition;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ */
+class FlowDecompositionResultsTests {
+    @Test
+    void testBuilderSave(){
+
+    }
+
+    @Test
+    void testBuilderCreateDecomposedFlow(){
+
+    }
+
+    @Test
+    void testBuilderBuild(){
+
+    }
+
+    @Test
+    void testGetters(){
+
+    }
+}
