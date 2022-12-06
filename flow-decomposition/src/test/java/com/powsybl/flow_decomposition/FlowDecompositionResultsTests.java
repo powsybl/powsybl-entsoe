7c0c65ba9700dd26eba25a9e0267604f886cc73c
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2022-12-06 14:33:28 +0100

    test FlowDecompositionResults
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

--- flow-decomposition/src/test/java/com/powsybl/flow_decomposition/FlowDecompositionResultsTests.java
+++ flow-decomposition/src/test/java/com/powsybl/flow_decomposition/FlowDecompositionResultsTests.java
@@ -12,6 +12,7 @@ import com.powsybl.iidm.network.Branch;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 import org.ejml.data.DMatrixSparseCSC;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -28,11 +29,43 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
  */
 class FlowDecompositionResultsTests {
+    private String branchId;
+    private String contingencyId2;
+    private String contingencyId3;
+    private Network network;
+    private XnecProviderByIds xnecProvider;
+    private FlowDecompositionResults flowDecompositionResults;
+    private DMatrixSparseCSC alloMatrix;
+    private DMatrixSparseCSC pstMatrix;
+    private Map<String, Integer> xnecMap;
+
+    @BeforeEach
+    void setup() {
+        String networkFileName = "19700101_0000_FO4_UX1.uct";
+        branchId = "DB000011 DF000011 1";
+        String contingencyElementId1 = "FB000011 FD000011 1";
+        String contingencyElementId2 = "FB000021 FD000021 1";
+        contingencyId2 = "DD000011 DF000011 1";
+        contingencyId3 = "FB000011 FD000011 1_FB000021 FD000021 1";
+
+        network = TestUtils.importNetwork(networkFileName);
+        xnecProvider = XnecProviderByIds.builder()
+            .addContingencies(Map.of(contingencyId2, Set.of(contingencyId2), contingencyId3, Set.of(contingencyElementId1, contingencyElementId2)))
+            .addNetworkElementsAfterContingencies(Set.of(branchId), Set.of(contingencyId2, contingencyId3))
+            .addNetworkElementsOnBasecase(Set.of(branchId))
+            .build();
+        flowDecompositionResults = new FlowDecompositionResults(network);
+
+        xnecMap = NetworkUtil.getIndex(List.of(branchId));
+        alloMatrix = new DMatrixSparseCSC(1, 2);
+        alloMatrix.set(0, 0, 20.0);
+        alloMatrix.set(0, 1, 12.0);
+        pstMatrix = new DMatrixSparseCSC(1, 1);
+        pstMatrix.set(0, 0, 2);
+    }
+
     @Test
     void testResultMetadata() {
-        String networkFileName = "19700101_0000_FO4_UX1.uct";
-        Network network = TestUtils.importNetwork(networkFileName);
-        FlowDecompositionResults flowDecompositionResults = new FlowDecompositionResults(network);
         assertEquals(network.getNameOrId(), flowDecompositionResults.getNetworkId());
         assertTrue(flowDecompositionResults.getId().startsWith("Flow_Decomposition_Results_of_"));
         Set<Country> zoneSet = flowDecompositionResults.getZoneSet();
@@ -43,48 +76,79 @@ class FlowDecompositionResultsTests {
     }
 
     @Test
-    void testBuilder() {
-        String networkFileName = "19700101_0000_FO4_UX1.uct";
-        String branchId = "DB000011 DF000011 1";
-        String contingencyElementId1 = "FB000011 FD000011 1";
-        String contingencyElementId2 = "FB000021 FD000021 1";
-        String contingencyId2 = "DD000011 DF000011 1";
-        String contingencyId3 = "FB000011 FD000011 1_FB000021 FD000021 1";
-
-        Network network = TestUtils.importNetwork(networkFileName);
-        XnecProviderByIds xnecProvider = XnecProviderByIds.builder()
-            .addContingencies(Map.of(contingencyId2, Set.of(contingencyId2), contingencyId3, Set.of(contingencyElementId1, contingencyElementId2)))
-            .addNetworkElementsAfterContingencies(Set.of(branchId), Set.of(contingencyId2, contingencyId3))
-            .addNetworkElementsOnBasecase(Set.of(branchId))
-            .build();
-        FlowDecompositionResults flowDecompositionResults = new FlowDecompositionResults(network);
+    void testBuilderNState() {
         List<Branch> nStateXnecList = xnecProvider.getNetworkElements(network);
-        List<Branch> n1StateContingency2XnecList = xnecProvider.getNetworkElements(contingencyId2, network);
-        List<Branch> n1StateContingency3XnecList = xnecProvider.getNetworkElements(contingencyId3, network);
         FlowDecompositionResults.PerStateBuilder nStateBuilder = flowDecompositionResults.getBuilder(nStateXnecList);
-        FlowDecompositionResults.PerStateBuilder n1StateBuilderContingency2 = flowDecompositionResults.getBuilder(contingencyId2, n1StateContingency2XnecList);
-        FlowDecompositionResults.PerStateBuilder n1StateBuilderContingency3 = flowDecompositionResults.getBuilder(contingencyId3, n1StateContingency3XnecList);
 
-        FlowDecompositionResults.PerStateBuilder builder = nStateBuilder;
-        builder.saveAcReferenceFlow(Map.of(branchId, 10.0));
-        builder.saveDcReferenceFlow(Map.of(branchId, 11.0));
-        DMatrixSparseCSC alloMatrix = new DMatrixSparseCSC(1, 2);
-        alloMatrix.set(0, 0, 20.0);
-        alloMatrix.set(0, 1, 12.0);
-        Map<String, Integer> xnecMap = NetworkUtil.getIndex(List.of(branchId));
-        builder.saveAllocatedAndLoopFlowsMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.ALLOCATED_COLUMN_NAME, 0, NetworkUtil.getLoopFlowIdFromCountry(FR), 1), alloMatrix));
-        DMatrixSparseCSC pstMatrix = new DMatrixSparseCSC(1, 1);
-        pstMatrix.set(0, 0, 2);
-        builder.savePstFlowMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.PST_COLUMN_NAME, 0), pstMatrix));
-        builder.build(FlowDecompositionParameters.DISABLE_RESCALED_RESULTS);
+        nStateBuilder.saveAcReferenceFlow(Map.of(branchId, 10.0));
+        nStateBuilder.saveDcReferenceFlow(Map.of(branchId, 11.0));
+        nStateBuilder.saveAllocatedAndLoopFlowsMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.ALLOCATED_COLUMN_NAME, 0, NetworkUtil.getLoopFlowIdFromCountry(FR), 1), alloMatrix));
+        nStateBuilder.savePstFlowMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.PST_COLUMN_NAME, 0), pstMatrix));
+        nStateBuilder.build(FlowDecompositionParameters.DISABLE_RESCALED_RESULTS);
+
         Map<String, DecomposedFlow> decomposedFlowMap = flowDecompositionResults.getDecomposedFlowMap();
         assertEquals(1, decomposedFlowMap.size());
+
         DecomposedFlow decomposedFlow = flowDecompositionResults.getDecomposedFlowMap().get(branchId);
+        assertEquals(branchId, decomposedFlow.getBranchId());
+        assertEquals("", decomposedFlow.getContingencyId());
+        assertEquals(branchId, decomposedFlow.getId());
         assertEquals(10.0, decomposedFlow.getAcReferenceFlow());
         assertEquals(11.0, decomposedFlow.getDcReferenceFlow());
         assertEquals(20.0, decomposedFlow.getAllocatedFlow());
         assertEquals(12.0, decomposedFlow.getLoopFlow(FR));
         assertEquals(2.0, decomposedFlow.getPstFlow());
-        //TODO
+    }
+
+    @Test
+    void testBuilderN1State() {
+        List<Branch> n1StateContingency2XnecList = xnecProvider.getNetworkElements(contingencyId2, network);
+        FlowDecompositionResults.PerStateBuilder n1StateBuilder = flowDecompositionResults.getBuilder(contingencyId2, n1StateContingency2XnecList);
+        String xnecId = "DB000011 DF000011 1_DD000011 DF000011 1";
+
+        n1StateBuilder.saveAcReferenceFlow(Map.of(branchId, 10.0));
+        n1StateBuilder.saveDcReferenceFlow(Map.of(branchId, 11.0));
+        n1StateBuilder.saveAllocatedAndLoopFlowsMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.ALLOCATED_COLUMN_NAME, 0, NetworkUtil.getLoopFlowIdFromCountry(FR), 1), alloMatrix));
+        n1StateBuilder.savePstFlowMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.PST_COLUMN_NAME, 0), pstMatrix));
+        n1StateBuilder.build(FlowDecompositionParameters.DISABLE_RESCALED_RESULTS);
+
+        Map<String, DecomposedFlow> decomposedFlowMap = flowDecompositionResults.getDecomposedFlowMap();
+        assertEquals(1, decomposedFlowMap.size());
+
+        DecomposedFlow decomposedFlow = flowDecompositionResults.getDecomposedFlowMap().get(xnecId);
+        assertEquals(branchId, decomposedFlow.getBranchId());
+        assertEquals(contingencyId2, decomposedFlow.getContingencyId());
+        assertEquals(xnecId, decomposedFlow.getId());
+        assertEquals(10.0, decomposedFlow.getAcReferenceFlow());
+        assertEquals(11.0, decomposedFlow.getDcReferenceFlow());
+        assertEquals(20.0, decomposedFlow.getAllocatedFlow());
+        assertEquals(12.0, decomposedFlow.getLoopFlow(FR));
+        assertEquals(2.0, decomposedFlow.getPstFlow());
+    }
+
+    @Test
+    void testBuilderN2State() {
+        List<Branch> n1StateContingency3XnecList = xnecProvider.getNetworkElements(contingencyId3, network);
+        FlowDecompositionResults.PerStateBuilder n2StateBuilder = flowDecompositionResults.getBuilder(contingencyId3, n1StateContingency3XnecList);
+        String xnecId = "DB000011 DF000011 1_FB000011 FD000011 1_FB000021 FD000021 1";
+
+        n2StateBuilder.saveAcReferenceFlow(Map.of(branchId, 10.0));
+        n2StateBuilder.saveDcReferenceFlow(Map.of(branchId, 11.0));
+        n2StateBuilder.saveAllocatedAndLoopFlowsMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.ALLOCATED_COLUMN_NAME, 0, NetworkUtil.getLoopFlowIdFromCountry(FR), 1), alloMatrix));
+        n2StateBuilder.savePstFlowMatrix(new SparseMatrixWithIndexesCSC(xnecMap, Map.of(DecomposedFlow.PST_COLUMN_NAME, 0), pstMatrix));
+        n2StateBuilder.build(FlowDecompositionParameters.DISABLE_RESCALED_RESULTS);
+
+        Map<String, DecomposedFlow> decomposedFlowMap = flowDecompositionResults.getDecomposedFlowMap();
+        assertEquals(1, decomposedFlowMap.size());
+
+        DecomposedFlow decomposedFlow = flowDecompositionResults.getDecomposedFlowMap().get(xnecId);
+        assertEquals(branchId, decomposedFlow.getBranchId());
+        assertEquals(contingencyId3, decomposedFlow.getContingencyId());
+        assertEquals(xnecId, decomposedFlow.getId());
+        assertEquals(10.0, decomposedFlow.getAcReferenceFlow());
+        assertEquals(11.0, decomposedFlow.getDcReferenceFlow());
+        assertEquals(20.0, decomposedFlow.getAllocatedFlow());
+        assertEquals(12.0, decomposedFlow.getLoopFlow(FR));
+        assertEquals(2.0, decomposedFlow.getPstFlow());
     }
 }
