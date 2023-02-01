be6b9eedcc067180ace3e60da1c78dc6e2110636
miovd <miora.ralambotiana@rte-france.com>
2023-02-01 11:01:54 +0100

    Implement AbstractVersion to retrieve entsoe version (#83)
    Signed-off-by: VEDELAGO MIORA <miora.ralambotiana@rte-france.com>

---
+++ commons/src/main/java-templates/com/powsybl/entsoe/commons/PowsyblEntsoeVersion.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+package com.powsybl.entsoe.commons;
+
+import com.google.auto.service.AutoService;
+import com.powsybl.tools.AbstractVersion;
+import com.powsybl.tools.Version;
+
+/**
+ * @author Miora Vedelago <miora.ralambotiana at rte-france.com>
+ */
+@AutoService(Version.class)
+public class PowsyblEntsoeVersion extends AbstractVersion {
+
+    public PowsyblEntsoeVersion() {
+        super("powsybl-entsoe", "${project.version}", "${buildNumber}", "${scmBranch}", Long.parseLong("${timestamp}"));
+    }
+}
