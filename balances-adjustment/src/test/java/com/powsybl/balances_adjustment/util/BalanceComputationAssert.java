c988ffb4b235a51dd169f0b8ddadcba171e253f4
georgebudau <138105777+georgebudau@users.noreply.github.com>
2023-07-17 11:11:02 +0200

    Added BalanceComputation reports (#106)
    Signed-off-by: George Budau <george.budau@artelys.com>
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>
    Co-authored-by: Bertrand Rix <bertrand.rix@artelys.com>

---
+++ balances-adjustment/src/test/java/com/powsybl/balances_adjustment/util/BalanceComputationAssert.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2023, Coreso SA (https://www.coreso.eu/) and TSCNET Services GmbH (https://www.tscnet.eu/)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.balances_adjustment.util;
+
+import com.google.common.io.ByteStreams;
+import com.powsybl.commons.reporter.ReporterModel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+
+import static com.powsybl.commons.test.TestUtil.normalizeLineSeparator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author George Budau {@literal <george.budau at artelys.com>}
+ */
+public final class BalanceComputationAssert {
+
+    private BalanceComputationAssert() {
+    }
+
+    public static void assertReportEquals(String refResourceName, ReporterModel reporter) throws IOException {
+        assertReportEquals(BalanceComputationAssert.class.getResourceAsStream(refResourceName), reporter);
+    }
+
+    public static void assertReportEquals(InputStream ref, ReporterModel reporter) throws IOException {
+        StringWriter sw = new StringWriter();
+        reporter.export(sw);
+
+        String refLogExport = normalizeLineSeparator(new String(ByteStreams.toByteArray(ref), StandardCharsets.UTF_8));
+        String logExport = normalizeLineSeparator(sw.toString());
+        assertEquals(refLogExport, logExport);
+    }
+}
