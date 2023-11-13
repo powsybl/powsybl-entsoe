b15e2f0e34806ea1ac611bdcc1c439bc84c450de
Guillaume Verger <guillaume.verger@artelys.com>
2023-11-13 12:06:29 +0100

    Add start and stop to controllers
    Signed-off-by: Guillaume Verger <guillaume.verger@artelys.com>

--- flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionObserver.java
+++ flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionObserver.java
@@ -6,13 +6,25 @@ import java.util.Map;
 
 public interface FlowDecompositionObserver {
 
+    void runStart();
+
+    void runDone();
+
     void computingBaseCase();
+
     void computingContingency(String contingencyId);
 
     void computedGlsk(Map<Country, Map<String, Double>> glsks);
 
     void computedNetPositions(Map<Country, Double> netPositions);
-    void computedNodalInjectionsMatrix(SparseMatrixWithIndexesTriplet nodalInjectionsMatrix);
-    void computedPtdfMatrix(SparseMatrixWithIndexesTriplet ptdfMatrix);
-    void computedPsdfMatrix(SparseMatrixWithIndexesTriplet matrix);
+
+    void computedNodalInjectionsMatrix(Map<String, Map<String, Double>> map);
+
+    void computedPtdfMatrix(Map<String, Map<String, Double>> map);
+
+    /**
+     * Called when the psdf matrix is ready
+     * @param map the matrix indexed by (line, node)
+     */
+    void computedPsdfMatrix(Map<String, Map<String, Double>> map);
 }
