7f3d057b8d12ffa4e828ed5a257e342a70a94088
Guillaume Verger <guillaume.verger@artelys.com>
2024-01-12 17:03:58 +0100

    add javadoc to observer interface
    Signed-off-by: Guillaume Verger <guillaume.verger@artelys.com>

--- flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionObserver.java
+++ flow-decomposition/src/main/java/com/powsybl/flow_decomposition/FlowDecompositionObserver.java
@@ -12,33 +12,89 @@ import java.util.Map;
 
 public interface FlowDecompositionObserver {
 
+    /**
+     * Called when the computation starts
+     */
     void runStart();
 
+    /**
+     * Called when the computation is done
+     */
     void runDone();
 
+    /**
+     * Called when the base case starts to be computed
+     */
     void computingBaseCase();
 
+    /**
+     * Called when starting to compute for the contingency
+     *
+     * @param contingencyId The current contingency id
+     */
     void computingContingency(String contingencyId);
 
+    /**
+     * Called when Glsk are computed thanks to the GlskProvider (during base case computation)
+     *
+     * @param glsks the Glsks per country per generator
+     */
     void computedGlsk(Map<Country, Map<String, Double>> glsks);
 
+    /**
+     * Called when net positions are computed (during base case computation)
+     *
+     * @param netPositions the net positions per country
+     */
     void computedNetPositions(Map<Country, Double> netPositions);
 
-    void computedNodalInjectionsMatrix(Map<String, Map<String, Double>> map);
-
-    void computedPtdfMatrix(Map<String, Map<String, Double>> map);
+    /**
+     * Called when the nodal injection matrix is computed (during n-state or contingency)
+     *
+     * @param nodalInjections the matrix of nodal injections indexed by(node, flow)
+     */
+    void computedNodalInjectionsMatrix(Map<String, Map<String, Double>> nodalInjections);
 
     /**
-     * Called when the psdf matrix is ready
-     * @param map the matrix indexed by (line, node)
+     * Called when the PTDF matrix is computed (during n-state or contingency)
+     *
+     * @param pdtfMatrix the matrix of ptdf indexed by (line, node)
      */
-    void computedPsdfMatrix(Map<String, Map<String, Double>> map);
+    void computedPtdfMatrix(Map<String, Map<String, Double>> pdtfMatrix);
 
+    /**
+     * Called when the psdf matrix is computed (during n-state or contingency)
+     *
+     * @param psdfMatrix the matrix of psdf indexed by (line, node)
+     */
+    void computedPsdfMatrix(Map<String, Map<String, Double>> psdfMatrix);
+
+    /**
+     * Called when the AC nodal injections matrix is computed (during n-state or contingency)
+     *
+     * @param positions the positions after AC loadflow
+     * @param fallbackHasBeenActivated true if AC loadflow didn't converge
+     */
     void computedAcNodalInjections(Map<String, Double> positions, boolean fallbackHasBeenActivated);
 
+    /**
+     * Called when the DC nodal injections matrix is computed (during n-state or contingency)
+     *
+     * @param positions the positions after DC loadflow
+     */
     void computedDcNodalInjections(Map<String, Double> positions);
 
+    /**
+     * Called when the AC loadflow is computed (during n-state or contingency)
+     *
+     * @param flows the flows for all branches
+     */
     void computedAcFlows(Map<String, Double> flows);
 
+    /**
+     * Called when the DC loadflow is computed (during n-state or contingency)
+     *
+     * @param flows the flows for all branches
+     */
     void computedDcFlows(Map<String, Double> flows);
 }
