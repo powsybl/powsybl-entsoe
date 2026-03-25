e78f23a3fd9790451d15b74d5d7fcd3e670df038
georg haider <georg.haider@artelys.com>
2026-03-25 16:39:22 +0100

    adapted to latest checkstyle rules

--- flow-decomposition/src/main/java/com/powsybl/flow_decomposition/utils/LogUtils.java
+++ flow-decomposition/src/main/java/com/powsybl/flow_decomposition/utils/LogUtils.java
@@ -5,49 +5,51 @@ import org.slf4j.LoggerFactory;
 
 public final class LogUtils {
 
-  private LogUtils() {
-    // empty
-  }
-
-  @FunctionalInterface
-  public interface ThrowingSupplier<T> {
-    T get() throws Throwable;
-  }
-
-  @FunctionalInterface
-  public interface ThrowingRunnable {
-    void run() throws Throwable;
-  }
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(LogUtils.class);
-
-  public static <T> T info(String s, ThrowingSupplier<T> suppl) {
-    long start = System.currentTimeMillis();
-    LOGGER.info("{} started", s);
-    T result = sneakyCall(suppl);
-    LOGGER.info("{} completed. Time={}", s, System.currentTimeMillis() - start);
-    return result;
-  }
-
-  public static void info(String s, ThrowingRunnable runn) {
-    info(s, () -> {
-      runn.run();
-      return null;
-    });
-  }
-
-  @SuppressWarnings("unchecked")
-  private static <T> T sneakyCall(ThrowingSupplier<T> run) {
-    try {
-      return run.get();
-    } catch (Throwable t) {
-      return (T) sneakyThrow(t);
+    private LogUtils() {
+        // empty
     }
-  }
 
-  @SuppressWarnings("unchecked")
-  private static <E extends Throwable, R> R sneakyThrow(Throwable t) throws E {
-    throw (E) t;
-  }
+    @FunctionalInterface
+    public interface ThrowingSupplier<T> {
+
+        T get() throws Throwable;
+    }
+
+    @FunctionalInterface
+    public interface ThrowingRunnable {
+
+        void run() throws Throwable;
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogUtils.class);
+
+    public static <T> T info(String s, ThrowingSupplier<T> suppl) {
+        long start = System.currentTimeMillis();
+        LOGGER.info("{} started", s);
+        T result = sneakyCall(suppl);
+        LOGGER.info("{} completed. Time={}", s, System.currentTimeMillis() - start);
+        return result;
+    }
+
+    public static void info(String s, ThrowingRunnable runn) {
+        info(s, () -> {
+            runn.run();
+            return null;
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T sneakyCall(ThrowingSupplier<T> run) {
+        try {
+            return run.get();
+        } catch (Throwable t) {
+            return (T) sneakyThrow(t);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <E extends Throwable, R> R sneakyThrow(Throwable t) throws E {
+        throw (E) t;
+    }
 
 }
